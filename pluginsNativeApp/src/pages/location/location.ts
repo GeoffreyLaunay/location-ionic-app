5b1a035e34c31bca5e724fb7aa6bb057e42f9102
glaunay <g.launay@alkante.com>
2018-10-03 16:09:05 +0200

    Ajout de la visualisation via Leaflet

--- pluginsNativeApp/src/pages/location/location.ts
+++ pluginsNativeApp/src/pages/location/location.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import * as L from "leaflet";
 
 @Component({
   selector: 'page-location',
@@ -12,11 +13,25 @@ export class LocationPage {
   public accuracy: any;
   public error: string;
 
+  map: L.Map;
+  center: L.PointTuple;
+
   constructor(
     public navCtrl: NavController,
     public geolocation: Geolocation
   ) {
 
+  }
+
+  ionViewDidLoad() {
+    this.map = L.map('leafletmap', {
+      center: [48.11, -1.53],
+      zoom:   13
+    });
+
+    var position = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Ma localisation'
+    }).addTo(this.map);
   }
 
   public getLocation() {
@@ -26,6 +41,11 @@ export class LocationPage {
       console.log(resp);
       this.coords   = resp.coords;
       this.accuracy = resp.coords.accuracy
+
+      L.marker([this.coords.latitude, this.coords.longitude]).addTo(this.map)
+      .bindPopup("Je suis ici !")
+      .openPopup();
+
     }).catch( (error) => {
       this.error = error;
       console.log("Error getting location", error);
